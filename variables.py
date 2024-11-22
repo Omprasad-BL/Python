40c8b305538a26fa42b0ad401d1457048a2162a9
Omprasadbl <omprasadbl@gmail.com>
2024-11-22 16:02:21 +0530

    variables chapter

---
+++ variables.py
@@ -0,0 +1,21 @@
+#its variable tutorials
+print("variables tutorial")
+a=10
+#variables should be smallercase
+#container to store data
+#global local variables
+
+a="2345"
+# you can change as per need and you can allocate different data type variable
+
+def var():
+    global x
+    x="Omprasad B L global variable"
+    print("hello world for method" )
+var()    
+
+print(x)
+
+print(type(a))
+#type checking. returns variable corresponding class
+
