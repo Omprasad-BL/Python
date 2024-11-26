2d13b47053a663dc9d19230b4ad4c381a7b25b83
Omprasadbl <omprasadbl@gmail.com>
2024-11-26 22:47:42 +0530

    pyhton math module for mathematical operations

---
+++ mathop.py
@@ -0,0 +1,31 @@
+#provides maths  methods to deal with basic mathematical operations
+
+
+import math
+#to find minimum 
+x=min(10,20,30)
+print(x)
+
+y=max(10,20,30)
+print(y)
+
+#abs used to convert any negative value to positive rounded values 
+print(abs(-10.00))
+
+#to find power of number
+print(pow(5,2))
+
+print(math.sqrt(200))
+
+# ceil rounds number in upword nearest number
+print(math.ceil(10.2))
+
+# floor rounds number in downword nearest number
+print(math.floor(10.2))
+
+
+# PI value
+print(math.pi)
+
+# to find factorial of number
+print( math.factorial(5))
