d43448bb66da823672f5fecac86401133e165f5a
Omprasadbl <omprasadbl@gmail.com>
2024-11-27 21:31:22 +0530

    pyhton math module for mathematical operations

--- mathop.py
+++ mathop.py
@@ -29,3 +29,33 @@ print(math.pi)
 
 # to find factorial of number
 print( math.factorial(5))
+
+
+#absolute for float number converts negative to positive
+print(math.fabs(-20.24))
+
+
+#method return sum of values in iterable object
+#any iterable object like list, tuple , set ..
+nums=[1,2,3,4,5,6,7,8,9,10]
+print(math.fsum(nums))
+
+
+#method to check is given real number is finite number or not
+print(math.isinf(float("inf")))
+
+#check is given real number is nan (not a number type or not)
+print(math.isnan(float("nan")))
+print(math.isnan(float(234)))
+
+
+print(math.inf)
+
+# to cut decimal points use this method
+print(math.trunc(23.43))
+
+
+
+
+
+
