160aa0c90117f961acfbae99896aa4af360642e9
Omprasadbl <omprasadbl@gmail.com>
2024-12-02 23:24:50 +0530

    input reader learnt

---
+++ pyinput.py
@@ -0,0 +1,11 @@
+name= input("enter a name")
+print(name)
+print({name})
+
+print(type(name))
+
+print(b'{name}')
+print(type(b'om'))
+# pyhton 2.7 --version
+# age=raw_input("Enter age")
+# print(int(age))
