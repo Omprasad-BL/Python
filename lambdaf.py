da5931f5524cef5d9d72ebd71e0e8c841af180a6
Omprasadbl <omprasadbl@gmail.com>
2024-12-07 23:56:53 +0530

    python  lambda

---
+++ lambdaf.py
@@ -0,0 +1,13 @@
+a=lambda a,b: a+b
+print(a(10,20))
+
+#LAMBDA functions made to work efficiently with less code
+# lamba functions return automatically if only one line within function
+# lambda functions accept n number of args seperated by comma 
+# lamba is small function used to work within another function
+
+def add(a,b):
+    return  a+b
+print(add(30,30))
+sub=lambda a,b:a-b
+print(sub(30,20))
