e3f4c3d5871ac0e810dd788a36e06520f0799174
Omprasadbl <omprasadbl@gmail.com>
2024-12-23 21:22:24 +0530

    lab programs added

---
+++ labset/dtype.py
@@ -0,0 +1,61 @@
+#int type
+x=1
+print("type of ",x, type(x))
+
+#string type
+x='1'
+print("type of ",x, type(x))
+
+#complex type
+x=2j
+print("type of ",x, type(x))
+
+#boolean type
+x=True
+print("type of ",x, type(x))
+
+#float type
+x=1.1
+print("type of ",x, type(x))
+
+#list type
+x=[1,2,3,4]
+print("type of ",x, type(x))
+
+#tuple type
+x=(1,2,3,4)
+print("type of ",x, type(x))
+
+#tuple with constructor
+x=tuple((1,2,3,4))
+print("type of ",x, type(x))
+
+#List with constructor
+x=list((1,2,3,4))
+print("type of ",x, type(x))
+
+x=set((1,2,3,4))
+print("type of ",x, type(x))
+
+#set data type
+x={1,2,3,5,6,}
+print("type of ",x, type(x))
+
+#frozen set with constructor
+x=frozenset((1,2,3,4))
+print("type of ",x, type(x))
+
+#dict with constructor
+x=dict({1:"om",2:"ram",3:"sham"})
+print("type of ",x, type(x))
+
+#dict without constructor
+x={1:"om",2:"ram",3:"sham"}
+print("type of ",x, type(x))
+
+x=b"omprasad"
+print("type of ",x, type(x))
+
+# x="hello"
+# print("type of ",x, type(bytearray(x)))
+
