4b82d8698bcae56409f6aae152c388c3e8e08d6c
Omprasadbl <omprasadbl@gmail.com>
2025-02-10 21:52:33 +0530

    pyhton list

---
+++ python_tutorial/List.py
@@ -0,0 +1,28 @@
+#Lists are used to store multiple items in a single variable.
+# allow duplicate values multiple datatypes index based 
+# add/remmove  last first 
+list=[1,2,3,4,5,6,6]
+
+print(list)
+
+# edit value
+list[2]=100
+print(list)
+
+# length of list
+print(len(list))
+
+# add element at end
+list.append(7)
+print(list)
+
+# remove element
+list.remove(7)
+print(list)
+
+# values can be     heterogeneous
+list1 = ["abc", 34, True, 40, "male"]
+print(list1)
+
+
+
