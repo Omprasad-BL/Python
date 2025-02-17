45f51e160f829e2c2766de208c9a30124ef36bc4
Omprasadbl <omprasadbl@gmail.com>
2025-02-17 08:53:39 +0530

    python math module

---
+++ python_tutorial/Math.py
@@ -0,0 +1,34 @@
+# python provides set of built in functions to deal with mathamatical works
+#  max(), min(), abs() are available in built in
+#  for more functionality you can use math library
+
+x=[1,2,3,4]
+# maximum
+print(max(x))
+
+# minimum
+print(min(x))
+
+# absolute value
+print(abs(-10))
+
+# factorial
+
+import math
+print(math.factorial(5))
+
+# round off number
+print(math.ceil(10.6))
+
+# floor off number
+print(math.floor(10.6))
+
+# power
+print(math.pow(x[3],5))
+
+# square root  
+print(math.sqrt(25))
+
+# pi
+print(math.pi)
+
