4273da3eb9ba26b8fd4ba8641a99211763b6efd9
Omprasadbl <omprasadbl@gmail.com>
2025-02-19 20:23:48 +0530

    python module

---
+++ python_tutorial/modRunner.py
@@ -0,0 +1,10 @@
+import module
+#  importing user created module and running functionality
+#  one single module can have many functions
+
+module.moduleMethod()
+
+
+#  to import only specific function from module
+from module import moduleMethod
+moduleMethod()
---
+++ python_tutorial/module.py
@@ -0,0 +1,5 @@
+def moduleMethod():
+    print("This is a method from a module.")
+    print("any operation can be done using perticular modules")
+
+    
