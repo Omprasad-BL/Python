98aa33383cc4156d04361168b79b52d5df2a096b
Omprasadbl <omprasadbl@gmail.com>
2025-02-23 16:45:08 +0530

    py int max min

---
+++ python_tutorial/Int.py
@@ -0,0 +1,11 @@
+import sys
+
+max_int = sys.maxsize
+min_int = -sys.maxsize - 1
+
+print(f"Maximum Integer: {max_int}")
+print(f"Minimum Integer: {min_int}")
+
+#For 32 bit systems these values will be.
+#Maximum Integer: 2147483647
+#Minimum Integer: -2147483648
