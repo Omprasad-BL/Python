34f1ac15d52cb274fbaa8020b13ab5037eb1e136
Omprasadbl <omprasadbl@gmail.com>
2025-02-24 17:21:48 +0530

    file handling in python

--- filehandle.py
+++ filehandle.py
@@ -1,24 +1,42 @@
-#file handling in pyhton
-#open() used to read and open file
-# constains 2 parameters 1st for file name and 2nd for file operations
-# ex open(hello.txt, "rt") here in 2nd parameter r means "read" t means "text" text is assigned by default
 
-f=open("iter.py","rt")
-#read is used to read  file 
-# print(f.read())
+# python file handling
+x=open("details.txt",'r')
+# read allows to read all content in file
+print (x.read())
+# you can also mention what kind data b or t means binary or text
 
-#prints only first 5 characters 
-# print(f.read(5))
+# you can print exact characters how much you want
+x.close()
+# to reopen you have to close it
+x=open("details.txt",'r')
+# print(x.read(5))
+print(x.readline())
+# to read only one line use readline() 
 
-#you can read line by line
-print(f.readline())
-print(f.readline())
+x.close()
+# to append data to existing file use a tag in open()
+x=open("details.txt",'a')
+
+x.write("\nNew Data")
+x.close()
+
+# after modification openning and adding details
+x=open("details.txt",'r')
+print(x.read())
+x.close()
+
+# to create new file use a tag in open() with x as second argument
+
+x=open("newfile.txt",'x')
+x.close()
 
 
-#iterating each line using for loop
-f1=open("iter.py","rt")
-for x in f1:
-    print(x)
-
-#to close the file
-f.close()    
+# to delete file use os module
+import os
+# x=os.remove("newfile.txt")
+# to remove folder use rmdir("filename")
+if os.path.exists('newfile.txt'):
+    print("File deleted successfully")
+    os.remove("newfile.txt")
+else:
+    print("The file does not exist")
