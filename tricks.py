a3f18c6c43dbfa2c335dc6ed311ab8d3544a0abd
Omprasadbl <omprasadbl@gmail.com>
2025-03-15 22:49:39 +0530

    tricks

--- tricks.py
+++ tricks.py
@@ -10,3 +10,30 @@ print(arr)
 fact=lambda x:x and  x*fact(x-1) or 1
 print(fact(5)) 
 
+# enumarations over element with proper indexing
+names = ['Alice', 'Bob', 'Cathy']
+
+for index, name in enumerate(names):
+    print(index, name)
+
+
+
+# zip method to zip 3 lists
+names = ['Alice', 'Bob', 'Cathy']
+ages = [25, 30, 35]
+
+paired = list(zip(names, ages))
+print(paired)  # Output: [('Alice', 25), ('Bob', 30), ('Cathy', 35)]
+
+# unzip zipped things
+ids=[1,2,3]
+grades=['A','B','C']
+zipped = zip(ids, names, grades)
+unzipped = list(zip(*zipped))
+print(unzipped)  # Output: [(1, 2, 3), ('Alice', 'Bob', 'Cathy'), ('A', 'B', 'A+')]
+
+numbers=ages
+# reverse a list
+sorted_numbers = sorted(numbers, reverse=True)
+print(sorted_numbers)  # Output: [9, 5, 4, 3, 2, 1, 1]
+
